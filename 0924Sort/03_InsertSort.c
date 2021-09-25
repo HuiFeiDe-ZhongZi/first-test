dc3c682ed0b792bcdf65d78c7078cf8b6862638d
shuaigeniu <1060718819@qq.com>
2021-09-25 08:51:31 +0800

    冒泡 选择 插入

--- 0924Sort/03_InsertSort.c
+++ 0924Sort/03_InsertSort.c
@@ -7,13 +7,13 @@ void InsertSort(int arr[],int len)
 	
 	int i;//有序
 	int j;//无序
-	int pTemp;//临时的保存变量
+	int temp;//临时的保存变量
 
 	//无序元素
 	for(i = 1;i<len;i++)
 	{
 		j=i-1;//有序的最后一个元素
-		temp = arr[j];//无序的第一个元素
+		temp = arr[i];//无序的第一个元素
 
 		while(arr[j] > temp && j>=0)
 		{
@@ -42,7 +42,7 @@ int main()
 {
 	int arr[] = {3,9,2,6,8,0,1,5};
 
-	SelectSort(arr,sizeof(arr)/sizeof(arr[0]));
+	InsertSort(arr,sizeof(arr)/sizeof(arr[0]));
 	Print(arr,sizeof(arr)/sizeof(arr[0]));
 
 	return 0;
