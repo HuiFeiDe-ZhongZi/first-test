259b6db97a7221b22a5904fca85862b467529f13
shuaigeniu <1060718819@qq.com>
2021-10-05 21:15:03 +0800

    信号屏蔽

---
+++ 1005Signal/sigsegv.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+
+int main()
+{
+	printf("ahhhahah\n");
+	printf("ahhhahah\n");
+	printf("ahhhahah\n");
+	printf("ahhhahah\n");
+	printf("ahhhahah\n");
+	printf("ahhhahah\n");
+	printf("ahhhahah\n");
+	printf("ahhhahah\n");
+	printf("ahhhahah\n");
+	printf("ahhhahah\n");
+	printf("ahhhahah\n");
+	printf("ahhhahah\n");
+	printf("ahhhahah\n");
+	printf("ahhhahah\n");
+	printf("ahhhahah\n");
+	printf("ahhhahah\n");
+	printf("ahhhahah\n");
+	printf("ahhhahah\n");
+	printf("ahhhahah\n");
+	printf("ahhhahah\n");
+	printf("ahhhahah\n");
+	printf("ahhhahah\n");
+	printf("ahhhahah\n");
+	printf("ahhhahah\n");
+
+	char *ptr = "ksdjf";
+	ptr[0] = 'A';
+
+	return 0;
+}
