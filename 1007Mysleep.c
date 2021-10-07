4ed4fdfcf7faddba72824fa1f5e5e55297604eec
shuaigeniu <1060718819@qq.com>
2021-10-07 21:01:37 +0800

    自定义实现sleep函数

---
+++ 1007Mysleep.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<string.h>
+#include<signal.h>
+
+void SIG_ALARM(int n)
+{
+	//空调用
+}
+
+unsigned int MYSLEEP(unsigned int seconds)
+{
+	unsigned int reval;
+	//信号屏蔽SIGALRM
+	sigset_t nset,oset;
+	sigemptyset(&nset);
+	sigaddset(&nset,SIGALRM);
+	sigprocmask(SIG_SETMASK,&nset,&oset);
+
+	//信号捕捉设定
+	struct sigaction act,oact;
+	act.sa_handler=SIG_ALARM;
+	act.sa_flags = 0;
+	sigemptyset(&act.sa_mask);
+	sigaction(SIGALRM,&act,&oact);
+	reval = alarm(seconds);//定时开始，定时结束发送SIGALRM
+	sleep(3);
+	sigsuspend(&oset);//pause() 挂起，等待信号(唤醒)
+
+	sigprocmask(SIG_SETMASK,&oset,NULL);//还原进程原有屏蔽字
+	sigaction(SIGALRM,&oact,NULL);//还原进程原有信号行为
+	return reval;
+}
+
+int main()
+{
+	while(1)
+	{
+		printf("two seconds...\n");
+		MYSLEEP(2);
+	}
+
+	return 0;
+}
