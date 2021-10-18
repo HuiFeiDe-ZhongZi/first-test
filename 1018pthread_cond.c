3ac40a6f20745bad31fc7e2a59b8ea4488ba4fed
shuaigeniu <1060718819@qq.com>
2021-10-18 21:06:23 +0800

    条件变量

---
+++ 1018pthread_cond.c
@@ -0,0 +1,69 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<string.h>
+#include<pthread.h>
+
+int work_cond; // 0(白天) 1(夜班)
+
+pthread_cond_t day_shift;
+pthread_cond_t night_shift;
+
+pthread_mutex_t lock; // 互斥锁
+
+void *day(void *arg)
+{
+	while(1)
+	{
+		pthread_mutex_lock(&lock);
+		if(work_cond)
+		{
+			// 夜班挂起
+			pthread_cond_wait(&day_shift,&lock);
+		}
+		printf("thread <0x%x> is on the day shift\n",(unsigned int)pthread_self());
+		work_cond = 1;
+		usleep(100000);
+		pthread_mutex_unlock(&lock);
+		pthread_cond_signal(&night_shift);
+	}
+}
+
+void *night(void *arg)
+{
+	while(1)
+	{
+		pthread_mutex_lock(&lock);
+		if(!work_cond)
+		{
+			// 夜班挂起
+			pthread_cond_wait(&night_shift,&lock);
+		}
+		printf("thread <0x%x> is on the night shift\n",(unsigned int)pthread_self());
+		work_cond = 0;
+		usleep(100000);
+		pthread_mutex_unlock(&lock);
+		pthread_cond_signal(&day_shift);
+	}
+}
+
+int main()
+{
+	pthread_t tid[2];
+
+
+	pthread_mutex_init(&lock,NULL);
+	pthread_cond_init(&day_shift,NULL);
+	pthread_cond_init(&night_shift,NULL);
+
+	pthread_create(&tid[0],NULL,day,NULL);
+	pthread_create(&tid[1],NULL,night,NULL);
+
+	int i = 2;
+	while(i--)
+	{
+		pthread_join(tid[i],NULL);
+	}
+
+	return 0;
+}
