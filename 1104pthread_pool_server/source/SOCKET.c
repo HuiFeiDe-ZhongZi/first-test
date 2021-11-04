71f7582f6df5a946edb1e70319f0da11b2d71d50
shuaigeniu <1060718819@qq.com>
2021-11-04 19:52:19 +0800

    “epoll+pool服务器”

---
+++ 1104pthread_pool_server/source/SOCKET.c
@@ -0,0 +1,61 @@
+#include "./SOCKET.h"
+
+int SOCKET(int domain, int type,int protocal)
+{
+	int reval;
+	if((reval = socket(domain,type,protocal)) == -1)
+	{
+		perror("SOCKET CREATE ERROR");
+		exit(-1);
+	}
+	return reval;
+}
+
+int BIND(int sockfd,const struct sockaddr * addr,socklen_t addrlen)
+{
+	int reval;
+	if((reval = bind(sockfd,addr,addrlen)) == -1)
+	{
+		perror("BIND CREATE ERROR");
+		exit(-1);
+	}
+	return reval;
+}
+
+int LISTEN(int sockfd,int backlog)
+{
+	int reval;
+	if((reval = listen(sockfd,backlog)) == -1)
+	{
+		perror("LISTEN CALL FAILED");
+		exit(-1);
+	}
+	return reval;
+}
+
+
+
+int ACCEPT(int sockfd,struct sockaddr * addr,socklen_t * addrlen)
+{
+	int reval;
+	if((reval = accept(sockfd,addr,addrlen)) == -1)
+	{
+		perror("ACCEPT CREATE ERROR");
+		exit(-1);
+	}
+	return reval;
+}
+
+int CONNECT(int sockfd,const struct sockaddr * addr,socklen_t addrlen)
+{
+	int reval;
+	if((reval = connect(sockfd,addr,addrlen)) == -1)
+	{
+		perror("CONNECT CALL FAILED");
+		exit(-1);
+	}
+	return reval;
+}
+
+
+
---
+++ 1104pthread_pool_server/source/main.c
@@ -0,0 +1,32 @@
+#include<pthread_pool.h>
+
+
+int main(int argc ,char **argv)
+{
+	if(argc < 4)
+	{
+		printf("Please presss thread_max and thread_min and queue_max!\n");
+		return 0;
+	}
+
+
+	thread_pool_t *pool = NULL;
+	int max = atoi(argv[1]);
+	int min = atoi(argv[2]);
+	int qmax = atoi(argv[3]);
+
+	int serverfd;
+
+	serverfd = thread_pool_net_init();
+	printf("服务器网路初始化成功...\n");
+	thread_pool_epoll_init(serverfd);
+	printf("服务器EPOLL初始化成功...\n");
+
+	pool = thread_pool_create(max,min,qmax);
+	printf("服务器线程池创建初始化成功...\n");
+
+	printf("服务器开始业务监听...\n");
+	thread_pool_epoll_listen(epfd,pool,serverfd);
+
+	return 0;
+}
